ff0d681a7dc7bfe98458fb931b2248de395d8fe9
bree256 <141923968+bree256@users.noreply.github.com>
2023-12-13 22:20:20 +0300

    change

--- loopArrayValue/loopValue.js
+++ loopArrayValue/loopValue.js
@@ -1,8 +1,9 @@
 //Build a simple loop, inside a function,
 //that takes an array and prints the value of the array to the console.
 function arrayLoopValue(array) {
-  for (i = 0; i < array.length; i++) i += 1;
-  console.log(i);
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
 }
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-arrayLoopValue(array);
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+arrayLoopValue(numbers);
